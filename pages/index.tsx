c6c3c85082d79e329a2b5e9d1e8f554ba83bab9c
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-04-12 00:40:54 -0300

    feat: Hydrates the component WorldMap
    Resolves #12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import dynamic from 'next/dynamic'
+
+import supabaseClient from '../lib/supabaseClient'
+import ApiData from '../lib/ApiData'
 
 const WorldMap = dynamic(import('../components/WorldMap/WorldMap'), {
   ssr: false
 })
 
-const Home: NextPage = () => (
-  <>
-    <WorldMap />
-  </>
-)
+const Home: NextPage = props => <WorldMap covidData={props['covidData']} />
+
+export const getStaticProps: GetStaticProps = async () => {
+  const date = '2021-03-22'
+  const { data: covid_daily_cases, error } = await supabaseClient
+    .from('covid_daily_cases')
+    .select('*')
+    .eq('date', date)
+
+  if (error) {
+    throw new Error(error.message)
+  }
+
+  const CovidApiData = new ApiData(covid_daily_cases)
+  CovidApiData.extractCountries()
+  const countries = CovidApiData.getCountries
+  countries.forEach(country => CovidApiData.clean(country))
+  const covidData = CovidApiData.getCleanData
+
+  return {
+    props: {
+      covidData
+    }
+  }
+}
 
 export default Home
