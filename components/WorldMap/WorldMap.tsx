952e580fbc0b462c0ecfe40317ef3afffbefd31d
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-04-12 00:36:44 -0300

    feat: The Map Mundi loads
    Add the react-plotly package to the project.
    Create IData interface.
    Resolves #10

--- components/WorldMap/WorldMap.tsx
+++ components/WorldMap/WorldMap.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Plot from 'react-plotly.js'
 
-const WorldMap = (): JSX.Element => (
+import IData from '../../Interfaces/IData'
+
+const unpack = (data, prop) => data.map(item => item[prop])
+
+const WorldMap = ({ covidData }: { covidData: Array<IData> }): JSX.Element => (
   <Plot
-    data={[{ type: 'choropleth', locationmode: 'country names' }]}
+    data={[
+      {
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: unpack(covidData, 'location'),
+        z: unpack(covidData, 'num_sequences_total'),
+        text: unpack(covidData, 'hover'),
+        hovertemplate:
+          '<i>País</i>: <b>%{location}</b><br><br>' +
+          '<i>Variantes</i>: <b>%{text}</b><br><br>' +
+          '<b>Total: %{z}</b>'
+      }
+    ]}
     layout={{
       title: 'Casos Diários de Covid-19 pelo Mundo',
       height: 768,
