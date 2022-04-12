d7901e584c2c73eb81ad9a47b03c9eb5f44e7cf7
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-04-12 17:26:01 -0300

    feat: Add and config the Material UI to the project

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createEmotionCache from '../src/lib/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
+}
